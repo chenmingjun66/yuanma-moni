309c1b4ae40f68768179d6af48f8958b0b2bede3
chenmingjun66 <1011693028@qq.com>
2019-11-19 16:55:25 +0800

    构造器注入

--- CMJSpring/src/main/java/com/chenmingjun/service/UserServiceImpl.java
+++ CMJSpring/src/main/java/com/chenmingjun/service/UserServiceImpl.java
@@ -5,6 +5,10 @@ import com.chenmingjun.dao.UserDao;
 public class UserServiceImpl implements UserService{
     UserDao dao;
 
+    public  UserServiceImpl(UserDao dao){
+        this.dao = dao;
+    }
+
     public UserDao getDao() {
         return dao;
     }
--- CMJSpring/src/main/java/org/spring/util/BeanFactory.java
+++ CMJSpring/src/main/java/org/spring/util/BeanFactory.java
@@ -6,6 +6,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.io.File;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -37,18 +38,17 @@ public class BeanFactory {
                 Element elementFirstChil = itFirst.next();
                 Attribute attributeId =elementFirstChil.attribute("id");
                 String beanName = attributeId.getValue();
-
                 Attribute attributeClass =elementFirstChil.attribute("class");
                 String clazzName = attributeClass.getValue();
                 Class clazz = Class.forName(clazzName);
-
-                Object object = clazz.newInstance();
 
                 /*
                 * 维护依赖关系
                 * 看这个对象有没有依赖（判断是否有property，或者判断类是否有属性）
                 * 如果有则注入
                 * */
+                Object object = null;
+
                 for (Iterator<Element> itSecond = elementFirstChil.elementIterator(); itSecond.hasNext();) {
                     //得到ref的value，通过value得到对象（map）
                     //得到name的值，然后根据值获取一个Filed的对象
@@ -56,18 +56,33 @@ public class BeanFactory {
 
                     Element elementSecondChil = itSecond.next();
                     if(elementSecondChil.getName().equals("property")) {
+
+                        //由于是setter，没有特殊的构造方法可以直接new
+                        object = clazz.newInstance();
                         String refValue = elementSecondChil.attribute("ref").getValue();
                         Object injectObject  = map.get(refValue);
                         String nameValue = elementSecondChil.attribute("name").getValue();
 
-
-
                         Field field = clazz.getDeclaredField(nameValue);
                         field.setAccessible(true);
                         field.set(object,injectObject);
+
+                    }else {
+                        //证明有特殊构造方法
+                        String refValue = elementSecondChil.attribute("ref").getValue();
+                        Object injectObject = map.get(refValue);
+                        Class injectObjectClazz = injectObject.getClass();
+                        Constructor constructor = clazz.getConstructor(injectObjectClazz.getInterfaces()[0]);
+                        object = constructor.newInstance(injectObject);
+
                     }
 
                 }
+                if (object == null){
+                    object = clazz.newInstance();
+
+                }
+
 
                 map.put(beanName,object);
 
